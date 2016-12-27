ded269150c01bdfa4a02d0483d2dfa345ed5e0be
Philip Kalikman <pkalikman@users.noreply.github.com>
2016-12-27 13:31:24 -0500

    Update new-type-generator.jl

--- examples/new-type-generator.jl
+++ examples/new-type-generator.jl
@@ -1,12 +1,15 @@
-using QuickCheck2, Base.Test
+using Checkers
 
-type mytype
+type ExampleType
 	parameter::Float64
 end
 
-QuickCheck2.custom_generator(a::Number, b::Number, type_a::Bool, type_b::Bool, mytype) = (size) -> mytype(a+(b-a)*rand(Float64))   ## Note `QuickCheck2` prefix!
+## Fully qualify 'custom_generator' if not explicitly importing from Checkers
+function Checkers.custom_generator(a::Number, b::Number, type_a::Bool, type_b::Bool, mytype)
+    (size) -> ExampleType(a+(b-a)*rand(Float64))   
+end
 
-@testset "new type generator test" begin  #
-	@test_formany 1<x::mytype<10, x.parameter==1
-	@test_formany 1<x::mytype<10, 10>x.parameter>1
+@testset "Testing custom generator for ExampleType" begin
+	@test_formany 1 < x::ExampleType < 10, x.parameter == 1
+	@test_formany 1 < x::ExampleType < 10, 1 < x.parameter < 10
 end
