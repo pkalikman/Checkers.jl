b437c4d585b30513aef3a7d316660500174427f9
Philip Kalikman <pkalikman@users.noreply.github.com>
2016-12-28 14:14:49 -0500

    Remove load of original quickcheck

--- src/Checkers.jl
+++ src/Checkers.jl
@@ -4,9 +4,6 @@ using Reexport
 @reexport using Base.Test
 import Base.Test: Result, Pass, Fail, Error, record, get_testset
 
-#TODO: Do we need to export these?
-#export property, condproperty, quantproperty, @not
-
 export @test_formany, @test_forall, @test_exists
 
 for (dir, filename) in [
@@ -15,8 +12,7 @@ for (dir, filename) in [
 		(".", "test-exists.jl"),
 		(".", "test-forall.jl"),
 		(".", "parse-argument-data.jl"),
-		(".", "custom-generator.jl"),
-		(".", "quickcheck-original.jl"),
+		(".", "custom-generator.jl")
 	]
 
 include(joinpath(dir, filename))
