22d1156b9267e0a115cd70412462856d96d13d75
Philip Kalikman <philip.kalikman@yale.edu>
2017-01-19 14:05:17 -0500

    Attempt at doc update for compatibility with RTD

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,18 @@
 using Documenter, Checkers
 
-makedocs(modules=[Checkers],doctest=true)
+makedocs(modules  = [Checkers],
+         doctest  = true,
+         clean    = false,
+         sitename = "Checkers.jl",
+         format   = Documenter.Formats.HTML,
+         pages    = Any["Introduction" = >"index.md",
+                        "Macros"=>Any["@test_forall"=>"test-forall.md"
+                                      "@test_exists"=>"test-exists.md"
+                                      "@test_formany"=>"test-formany.md"],
+                        "About"=>Any["License"=>"LICENSE.md"]]
+        )
 
 deploydocs(deps   = Deps.pip("mkdocs","python-markdown-math"),
            repo   = "github.com/pkalikman/Checkers.jl.git",
-           julia  = "0.5.0",
-           osname = "osx")
+           target = "build",
+           julia  = "0.5.0")
