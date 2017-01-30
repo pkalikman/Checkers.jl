cbe0ce9937d4fcd1bbd43529f9ef4e07a71d99f6
Philip Kalikman <pkalikman@users.noreply.github.com>
2017-01-30 15:28:05 -0500

    Update make.jl to try to get RTD working

--- docs/make.jl
+++ docs/make.jl
@@ -5,14 +5,20 @@ makedocs(modules  = [Checkers],
          clean    = false,
          sitename = "Checkers.jl",
          format   = Documenter.Formats.HTML,
-         pages    = Any["Introduction" = >"index.md",
-                        "Macros"=>Any["@test_forall"=>"test-forall.md"
-                                      "@test_exists"=>"test-exists.md"
-                                      "@test_formany"=>"test-formany.md"],
-                        "About"=>Any["License"=>"LICENSE.md"]]
-        )
+         pages    = Any[
+             "Introduction" => "index.md",
+             "Macros"       => Any[
+                 "@test_forall"  => "macros/test-forall.md",
+                 "@test_exists"  => "macros/test-exists.md",
+                 "@test_formany" => "macros/test-formany.md"
+              ],
+              "About"       => Any[
+                 "License"       => "LICENSE.md"
+              ]
+         ]
+)
 
-deploydocs(deps   = Deps.pip("mkdocs","python-markdown-math"),
+deploydocs(#deps   = Deps.pip("mkdocs","python-markdown-math"),
            repo   = "github.com/pkalikman/Checkers.jl.git",
            target = "build",
            julia  = "0.5.0")
