f5b596a843039997e2852cb6148188cab23992ac
Philip Kalikman <pkalikman@users.noreply.github.com>
2017-02-13 15:51:07 -0500

    More build docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,9 @@
 using Documenter, Checkers
 
 makedocs(modules  = [Checkers],
-         doctest  = true,
-         clean    = false,
-         sitename = "Checkers.jl",
-         format   = Documenter.Formats.HTML,
-         pages    = Any[
-             "Introduction" => "index.md",
-             "Macros"       => Any[
-                 "@test_forall"  => "macros/test-forall.md",
-                 "@test_exists"  => "macros/test-exists.md",
-                 "@test_formany" => "macros/test-formany.md"
-              ],
-              "About"       => Any[
-                 "License"       => "LICENSE.md"
-              ]
-         ]
-)
+         doctest  = false)
 
-deploydocs(deps   = Deps.pip("mkdocs","python-markdown-math"),
+deploydocs(deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", "pygments"),
            repo   = "github.com/pkalikman/Checkers.jl.git",
            target = "build",
            julia  = "0.5",
