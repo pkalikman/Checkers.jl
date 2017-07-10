b767006361daa743591f20c6d962a9dda0aedea2
Philip Kalikman <pkalikman@users.noreply.github.com>
2017-07-10 19:25:12 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,17 @@
 using Documenter, Checkers
 
-makedocs(modules  = [Checkers])
+makedocs(modules  = [Checkers],
+         doctest  = false,
+         clean    = true,
+         sitename = "Checkers.jl",
+         authors  = "Philip Kalikman, Efim Abrikosov",
+         pages    = Any[
+                        "Home" => "index.md",
+                        "Macros" => Any["macros/test-forall.md",
+                                        "macros/test-formany.md",
+                                        "macros/test-exists.md"]
+                    ]
+)
 
 deploydocs(deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", "pygments"),
            repo   = "github.com/pkalikman/Checkers.jl.git",
